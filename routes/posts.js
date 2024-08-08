f3ba6550691d62c43e128c3a54048911bfaac077
Aakanksh Singh <aakanksh.singh@indiahealthlink.com>
2024-08-08 09:40:11 +0530

    implemented backend logics for forgot create posts and all

--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const Post = require("../models/post");
 const authMiddleware = require("../middleware/authMiddleware");
+const upload = require("../utils/upload");
 const router = express.Router();
 
-router.post("/post", authMiddleware, async (req, res) => {
-  const { title, content } = req.body;
-  try {
-    const post = new Post({
-      title,
-      content,
-      authorId: req.user.id,
-      authorName: req.user.name,
-    });
-    await post.save();
-    res.status(201).json({ message: "Post created successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Post creation failed" });
+router.post(
+  "/post",
+  authMiddleware,
+  upload.single("image"),
+  async (req, res) => {
+    const { title, content } = req.body;
+    try {
+      const post = new Post({
+        title,
+        content,
+        authorId: req.user.id,
+        image: req.file ? req.file.path : null,
+      });
+      await post.save();
+      res.status(201).json({ message: "Post created successfully" });
+    } catch (error) {
+      res.status(500).json({ error: "Post creation failed" });
+    }
   }
-});
+);
 
 router.get("/posts", async (req, res) => {
   try {
@@ -30,8 +36,6 @@ router.get("/posts", async (req, res) => {
   }
 });
 
-module.exports = router;
-
 router.get("/posts/:id", async (req, res) => {
   try {
     const { id } = req.params;
@@ -39,9 +43,10 @@ router.get("/posts/:id", async (req, res) => {
     if (!post) {
       return res.status(404).json({ error: "Post not found" });
     }
-    res.json(post.content);
+    res.json(post);
   } catch (error) {
     res.status(500).json({ error: "Fetching post failed" });
   }
-});  
+});
 
+module.exports = router;
